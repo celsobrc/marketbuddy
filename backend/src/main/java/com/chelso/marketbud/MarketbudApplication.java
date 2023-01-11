d383c69bc1cfbc8738755a2210135ae8b6a86e08
Celso Zabala <chelso@MacBook-Air-de-Celso.local>
2023-01-11 19:17:46 -0300

    Initial Commit

---
+++ backend/src/main/java/com/chelso/marketbud/MarketbudApplication.java
@@ -0,0 +1,13 @@
+package com.chelso.marketbud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketbudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketbudApplication.class, args);
+	}
+
+}
